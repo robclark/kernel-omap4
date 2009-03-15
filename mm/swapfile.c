4d6bc2dc1cabfa75b218003a747bc64a4b87b862
Tim Gardner <tim.gardner@canonical.com>
2009-03-15 13:38:17 -0600

    UBUNTU: SAUCE: (no-up) swap: Add notify_swap_entry_free callback for compcache
    Code is required for ubuntu/compcache
    
    Signed-off-by: Ben Collins <ben.collins@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -594,6 +594,12 @@ static unsigned char swap_entry_free(struct swap_info_struct *p,
 			disk->fops->swap_slot_free_notify(p->bdev, offset);
 	}
 
+	if (!swap_count(count)) {
+		mem_cgroup_uncharge_swap(entry);
+			if (p->notify_swap_entry_free_fn)
+				p->notify_swap_entry_free_fn(offset);
+	}
+
 	return usage;
 }
 
@@ -2292,6 +2298,23 @@ int swapcache_prepare(swp_entry_t entry)
 {
 	return __swap_duplicate(entry, SWAP_HAS_CACHE);
 }
+
+/*
+ * Sets callback for event when swap_map[offset] == 0
+ * i.e. page at this swap offset is not longer used.
+ *
+ * type: identifies swap file
+ * fn: callback function
+ */
+void set_notify_swap_entry_free(unsigned type, void (*fn) (unsigned long))
+{
+	struct swap_info_struct *sis;
+	sis = swap_info[type];
+	BUG_ON(!sis);
+	sis->notify_swap_entry_free_fn = fn;
+	return;
+}
+EXPORT_SYMBOL(set_notify_swap_entry_free);
 
 /*
  * swap_lock prevents swap_map being freed. Don't grab an extra
