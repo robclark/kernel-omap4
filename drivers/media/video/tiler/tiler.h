ce1491d74a5d0c6a1ce43387cac584de80f6bb1f
David Sin <davidsin@ti.com>
2010-01-19 10:51:24 -0600

    TILER: Replace page stack logic with new one using kernel lists
    Add wrapper structure to house buf_info struct to fix copy to/from
    issues
    
    Signed-off-by: David Sin <davidsin@ti.com>

--- drivers/media/video/tiler/tiler.h
+++ drivers/media/video/tiler/tiler.h
@@ -71,7 +71,6 @@ struct tiler_buf_info {
 	s32 num_blocks;
 	struct tiler_block_info blocks[TILER_MAX_NUM_BLOCKS];
 	s32 offset;
-	struct list_head list; /* always last */
 };
 
 s32 tiler_alloc(enum tiler_fmt fmt, u32 width, u32 height, u32 *sys_addr);
