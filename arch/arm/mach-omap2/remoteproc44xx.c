d99219097b771c099600b481efcdcc50166c4d31
Hari Kanigeri <h-kanigeri2@ti.com>
2010-09-09 13:04:39 -0500

    omap:remoteproc-Initial support for remote proc
    This patch provides the inital series of commits for
    remote proc on omap
    
    omap:remoteproc-add remoteproc support
    
    Add remote proc support
    
    Signed-off-by: Hari Kanigeri <h-kanigeri2@ti.com>
    Signed-off-by: Ohad Ben-Cohen <ohad@wizery.com>
    Singed-off-by: Angela Stegmaier <angelabaker@ti.com>
    
    omap:remoteproc-provide multi-omap support
    
    This patch resolves the build issues when trying to build with
    multi-omap configuration.
    
    Signed-off-by: Hari Kanigeri <h-kanigeri2@ti.com>
    
    omap:remoteproc-add mechanism to enable bios timer
    
    BIOS requires the GPTimer for handling the OS activities.
    This is to configuer the GPTimers that are required for
    the remote processors.
    
    Signed-off-by: Hari Kanigeri <h-kanigeri2@ti.com>
    Signed-off-by: Fernando Guzman Lugo <x0095840@ti.com>
    
    syslink: rproc - add support for START and STOP events
    
    Now apps can wait for remote proc START and STOP events
    
    Signed-off-by: Fernando Guzman Lugo <x0095840@ti.com>
    
    rproc: fix missing common proc args to match userspace
    
    Add the common proc args structure to store the
    status of the api in order to match userspace
    side.
    
    Signed-off-by: Fernando Guzman Lugo <x0095840@ti.com>
    
    rproc: add protection to rproc_eventdf_ntfy
    
    The list of fd handles most be protected while traversing
    
    Signed-off-by: Fernando Guzman Lugo <x0095840@ti.com>
    
    omap:remoteproc: disable gpt3&4 as bios timer source
    
    Each core sys/app were requesting gpt3/4, respectively,
    as bios timer source clock. This patch is disabling the use
    of those timers as bios timer source since now the internal
    M3 timers are set for this purpose. Anyway the mechanism to
    set any timer as bios timer source was kept.
    
    If a gptimer is set as bios timer source changes in Ducati side
    are needed.
    
    Signed-off-by: Miguel Vadillo <vadillo@ti.com>
    
    omap:remoteproc: enable gpt3&4 for ipu hibernation
    
    gptimer 3/4 are set to be use at the hibernation timers for IPU,
    both clocks are enabled and configured in rproc_start and disable
    in rproc_stop.
    
    When mpu is hibernating IPU the iommu and mbox context will be
    saved and then rproc_sleep will be called to disable each core and
    also the associated gptimer needed to allowed retention. Whenever a
    message is sent to IPU the ipu_pm_restore will check and wake up
    ipu, if needed, restoring the iommu and mbox context and calling
    rproc_wake that will enable ipu and the associated gptimer again.
    
    When disable, the hibernating process will only happen if the
    system suspend/resume path is sent, IPU self hibernation wont be
    executed. Also gpt3/4 won't be requested at all.
    
    Signed-off-by: Miguel Vadillo <vadillo@ti.com>
    Signed-off-by: Juan Gutierrez <jgutierrez@ti.com>
    Signed-off-by: Paul Hunt <hunt@ti.com>
    
    omap: remoteproc - fix sparse warnings in remote proc about static
    
    This patch fixes these warnings:
    
    arch/arm/plat-omap/remoteproc.c:45:6:
    warning: symbol 'rproc_eventfd_ntfy' was not declared. Should it be static?
    arch/arm/plat-omap/remoteproc.c:180:5:
    warning: symbol 'rproc_reg_user_event' was not declared. Should it be static?
    arch/arm/plat-omap/remoteproc.c:227:5:
    warning: symbol 'rproc_unreg_user_event' was not declared. Should it be static?
    
    Change-Id: Ie822bbf97dc9dbf4b76bff8e115b3e9063037710
    Signed-off-by: Fernando Guzman Lugo <x0095840@ti.com>
    
    omap: remoteproc: CONFIG flag for self hibernation
    
    A new CONFIG flag is introduce to control IPU self hibernation
    the name of the new flag is CONFIG_SYSLINK_IPU_SELF_HIBERNATION
    when disable the timers used by IPU to hibernate and
    the flag in sh mem to allow hibernation of IPU are not set and IPU
    will hibernate only when sending the system suspend.
    On the other hand if the flag is set after N seconds of inactivity
    and if there is no resource requested by IPU (using ipu_pm) IPU will
    hibernate.
    
    Change-Id: I3f14257930a861c8784d7159a821228111b3ba37
    Signed-off-by: Miguel Vadillo <vadillo@ti.com>

---
+++ arch/arm/mach-omap2/remoteproc44xx.c
@@ -0,0 +1,255 @@
+/*
+ * Remote Processor machine-specific module for OMAP3
+ *
+ * Copyright (C) 2010 Texas Instruments Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+#include <linux/kernel.h>
+#include <linux/clk.h>
+#include <linux/err.h>
+#include <linux/platform_device.h>
+#include <linux/io.h>
+#include <plat/remoteproc.h>
+#include <plat/dmtimer.h>
+
+#include <plat/omap_device.h>
+#include <plat/omap_hwmod.h>
+
+
+static inline int proc44x_start(struct device *dev, u32 start_addr)
+{
+	struct platform_device *pdev = to_platform_device(dev);
+	struct omap_rproc *obj = (struct omap_rproc *)platform_get_drvdata(
+						to_platform_device(dev));
+	int ret = 0;
+
+	/* Enable the Timer that would be used by co-processor */
+	if (obj->timer_id >= 0) {
+		obj->dmtimer =
+			omap_dm_timer_request_specific(obj->timer_id);
+		if (!obj->dmtimer) {
+			ret = -EBUSY;
+			goto err_start;
+		}
+		omap_dm_timer_set_int_enable(obj->dmtimer,
+						OMAP_TIMER_INT_OVERFLOW);
+		omap_dm_timer_set_source(obj->dmtimer, OMAP_TIMER_SRC_SYS_CLK);
+	}
+
+	ret = omap_device_enable(pdev);
+	if (ret)
+		goto err_start;
+
+	obj->state = OMAP_RPROC_RUNNING;
+	return 0;
+
+err_start:
+	dev_err(dev, "%s error 0x%x\n", __func__, ret);
+	return ret;
+}
+
+static inline int proc44x_stop(struct device *dev)
+{
+	struct platform_device *pdev = to_platform_device(dev);
+	struct omap_rproc *obj = (struct omap_rproc *)platform_get_drvdata(
+						to_platform_device(dev));
+	int ret = 0;
+
+	if (obj->state == OMAP_RPROC_RUNNING) {
+		ret = omap_device_shutdown(pdev);
+		if (ret)
+			dev_err(dev, "%s err 0x%x\n", __func__, ret);
+
+		if (obj->dmtimer) {
+			omap_dm_timer_free(obj->dmtimer);
+			obj->dmtimer = NULL;
+		}
+	}
+
+	obj->state = OMAP_RPROC_STOPPED;
+	return ret;
+}
+
+static inline int proc44x_sleep(struct device *dev)
+{
+	struct platform_device *pdev = to_platform_device(dev);
+	struct omap_rproc *obj = (struct omap_rproc *)platform_get_drvdata(
+						to_platform_device(dev));
+	int ret = 0;
+
+	if (obj->state == OMAP_RPROC_RUNNING) {
+		ret = omap_device_shutdown(pdev);
+		if (ret)
+			dev_err(dev, "%s err 0x%x\n", __func__, ret);
+
+		if (obj->dmtimer)
+			omap_dm_timer_disable(obj->dmtimer);
+	}
+
+	obj->state = OMAP_RPROC_HIBERNATING;
+	return ret;
+}
+
+static inline int proc44x_wakeup(struct device *dev)
+{
+	struct platform_device *pdev = to_platform_device(dev);
+	struct omap_rproc *obj = (struct omap_rproc *)platform_get_drvdata(
+						to_platform_device(dev));
+	int ret = 0;
+
+	if (obj->dmtimer)
+		omap_dm_timer_enable(obj->dmtimer);
+
+	ret = omap_device_enable(pdev);
+	if (ret)
+		goto err_start;
+
+	obj->state = OMAP_RPROC_RUNNING;
+	return 0;
+
+err_start:
+	dev_err(dev, "%s error 0x%x\n", __func__, ret);
+	return ret;
+}
+
+
+static inline int omap4_rproc_get_state(struct device *dev)
+{
+	struct platform_device *pdev = to_platform_device(dev);
+	struct omap_device *odev = to_omap_device(pdev);
+
+	return odev->_state;
+}
+
+static struct omap_rproc_ops omap4_ducati0_ops = {
+	.start = proc44x_start,
+	.stop = proc44x_stop,
+	.sleep = proc44x_sleep,
+	.wakeup = proc44x_wakeup,
+	.get_state = omap4_rproc_get_state,
+};
+
+static struct omap_rproc_ops omap4_ducati1_ops = {
+	.start = proc44x_start,
+	.stop = proc44x_stop,
+	.sleep = proc44x_sleep,
+	.wakeup = proc44x_wakeup,
+	.get_state = omap4_rproc_get_state,
+};
+
+static struct omap_rproc_ops omap4_tesla_ops = {
+	.start = proc44x_start,
+	.stop = proc44x_stop,
+	.sleep = proc44x_sleep,
+	.wakeup = proc44x_wakeup,
+	.get_state = omap4_rproc_get_state,
+};
+
+static struct omap_rproc_platform_data omap4_rproc_data[] = {
+	{
+		.name = "tesla",
+		.ops = &omap4_tesla_ops,
+		.oh_name = "dsp_c0",
+		.timer_id = 5,
+	},
+	{
+		.name = "ducati-proc0",
+		.ops = &omap4_ducati0_ops,
+		.oh_name = "ipu_c0",
+#ifdef CONFIG_SYSLINK_IPU_SELF_HIBERNATION
+		.timer_id = 3,
+#else
+		.timer_id = -1,
+#endif
+	},
+	{
+		.name = "ducati-proc1",
+		.ops = &omap4_ducati1_ops,
+		.oh_name = "ipu_c1",
+#ifdef CONFIG_SYSLINK_IPU_SELF_HIBERNATION
+		.timer_id = 4,
+#else
+		.timer_id = -1,
+#endif
+
+	},
+};
+
+static struct omap_device_pm_latency omap_rproc_latency[] = {
+	{
+		.deactivate_func = omap_device_idle_hwmods,
+		.activate_func	 = omap_device_enable_hwmods,
+		.deactivate_lat = 1,
+		.activate_lat = 1,
+	},
+};
+
+struct omap_rproc_platform_data *omap4_get_rproc_data(void)
+{
+	return omap4_rproc_data;
+}
+
+
+#define NR_RPROC_DEVICES ARRAY_SIZE(omap4_rproc_data)
+
+static struct omap_device *omap4_rproc_pdev[NR_RPROC_OMAP4_DEVICES];
+
+static int __init omap4_rproc_init(void)
+{
+	struct omap_hwmod *oh;
+	struct omap_device_pm_latency *ohl;
+	char *oh_name, *pdev_name;
+	int ohl_cnt = 0, i;
+	int rproc_data_size;
+	struct omap_rproc_platform_data *rproc_data;
+
+	pdev_name = "omap-remoteproc";
+	ohl = omap_rproc_latency;
+	ohl_cnt = ARRAY_SIZE(omap_rproc_latency);
+
+
+	rproc_data = omap4_get_rproc_data();
+	rproc_data_size = NR_RPROC_OMAP4_DEVICES;
+
+	for (i = 0; i < rproc_data_size; i++) {
+		oh_name = rproc_data[i].oh_name;
+		oh = omap_hwmod_lookup(oh_name);
+		if (!oh) {
+			pr_err("%s: could not look up %s\n", __func__, oh_name);
+			continue;
+		}
+		omap4_rproc_pdev[i] = omap_device_build(pdev_name, i, oh,
+					&rproc_data[i],
+					sizeof(struct omap_rproc_platform_data),
+					ohl, ohl_cnt, false);
+		WARN(IS_ERR(omap4_rproc_pdev[i]), "Could not build omap_device"
+				"for %s %s\n", pdev_name, oh_name);
+	}
+	return 0;
+}
+module_init(omap4_rproc_init);
+
+static void __exit omap4_rproc_exit(void)
+{
+
+}
+module_exit(omap4_rproc_exit);
+
+MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("OMAP4 Remote Processor module");
+MODULE_AUTHOR("Ohad Ben-Cohen <ohad@wizery.com>");
+MODULE_AUTHOR("Hari Kanigeri <h-kanigeri2@ti.com>");
---
+++ arch/arm/plat-omap/include/plat/remoteproc.h
@@ -0,0 +1,147 @@
+/*
+ * OMAP Remote Processor driver
+ *
+ * Copyright (C) 2010 Texas Instruments Inc.
+ *
+ * Written by Ohad Ben-Cohen <ohad@wizery.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+
+#ifndef REMOTEPROC_H
+#define REMOTEPROC_H
+
+#include <linux/ioctl.h>
+#include <linux/cdev.h>
+
+#define RPROC_IOC_MAGIC		'P'
+
+#define RPROC_IOCMONITOR	_IO(RPROC_IOC_MAGIC, 0)
+#define RPROC_IOCSTART		_IO(RPROC_IOC_MAGIC, 1)
+#define RPROC_IOCSTOP		_IO(RPROC_IOC_MAGIC, 2)
+#define RPROC_IOCGETSTATE	_IOR(RPROC_IOC_MAGIC, 3, int)
+#define RPROC_IOCREGEVENT	_IOR(RPROC_IOC_MAGIC, 4, \
+					struct omap_rproc_reg_event_args)
+#define RPROC_IOCUNREGEVENT	_IOR(RPROC_IOC_MAGIC, 5, \
+					struct omap_rproc_reg_event_args)
+
+#define RPROC_IOC_MAXNR		(5)
+
+#ifdef CONFIG_ARCH_OMAP4
+#define NR_RPROC_OMAP4_DEVICES 3
+#else
+#define NR_RPROC_OMAP4_DEVICES 0
+#endif
+
+struct omap_rproc;
+
+struct omap_rproc_ops {
+	int (*start)(struct device *dev, u32 start_addr);
+	int (*stop)(struct device *dev);
+	int (*sleep)(struct device *dev);
+	int (*wakeup)(struct device *dev);
+	int (*get_state)(struct device *dev);
+};
+
+struct omap_rproc_clk_t {
+	void *clk_handle;
+	const char *dev_id;
+	const char *con_id;
+};
+
+/* RPROC events. */
+enum {
+	OMAP_RPROC_START,
+	OMAP_RPROC_STOP,
+};
+
+/* RPROC states. */
+enum {
+	OMAP_RPROC_STOPPED,
+	OMAP_RPROC_RUNNING,
+	OMAP_RPROC_HIBERNATING,
+};
+
+enum {
+	PROC_ERROR = 1,
+	PROC_STOP,
+	PROC_START,
+};
+
+struct omap_rproc_common_args {
+	int status;
+};
+
+
+struct omap_rproc_platform_data {
+	struct omap_rproc_ops *ops;
+	char *name;
+	char *oh_name;
+	int timer_id;
+};
+
+struct omap_rproc {
+	const char *name;
+	struct device *dev;
+	struct cdev cdev;
+	atomic_t count;
+	int state;
+	int minor;
+	struct blocking_notifier_head	notifier;
+	struct mutex lock;
+	int timer_id;
+	struct omap_dm_timer *dmtimer;
+	struct list_head event_list;
+	spinlock_t event_lock;
+};
+
+struct omap_rproc_ntfy {
+	struct list_head list;
+	int fd;
+	struct eventfd_ctx *evt_ctx;
+	u32 event;
+};
+
+struct omap_rproc_start_args {
+	u32 start_addr;
+};
+
+struct omap_rproc_reg_event_args {
+	struct omap_rproc_common_args cargs;
+	u16 pro_id;
+	int fd;
+	u32 event;
+};
+
+extern int rproc_start(struct omap_rproc *rproc, const void __user *arg);
+extern int rproc_stop(struct omap_rproc *rproc);
+extern int rproc_sleep(struct omap_rproc *rproc);
+extern int rproc_wakeup(struct omap_rproc *rproc);
+
+extern int omap_rproc_register_notifier(struct omap_rproc *rproc,
+					struct notifier_block *nb);
+extern int omap_rproc_unregister_notifier(struct omap_rproc *rproc,
+					struct notifier_block *nb);
+extern int omap_rproc_notify_event(struct omap_rproc *obj, int event,
+								void *data);
+
+extern struct omap_rproc *omap_rproc_get(const char *name);
+extern void omap_rproc_put(struct omap_rproc *obj);
+
+struct omap_rproc_platform_data *omap3_get_rproc_data(void);
+int omap3_get_rproc_data_size(void);
+struct omap_rproc_platform_data *omap4_get_rproc_data(void);
+#endif /* REMOTEPROC_H */
