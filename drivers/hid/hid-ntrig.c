ddf3bd0def68f3a5e2619e38626d00a940f2559c
Henrik Rydberg <rydberg@euromail.se>
2010-09-10 16:03:45 +0200

    UBUNTU: SAUCE: hid: ntrig: Correct logic for quirks
    The driver data is a bitmask, check for correct bits.
    
    Has been tested succesfully on Dell Studio 17, Dell XT2, HP TX2 and
    Lenovo T410s. Not likely to go upstream in its present form. However,
    it does help constitue a tuning-free driver which works well together
    with Unity, and as such are vital to the MT push.
    
    Signed-off-by: Henrik Rydberg <rydberg@euromail.se>
    Acked-by: Chase Douglas <chase.douglas@canonical.com>
    Signed-off-by: Leann Ogasawara <leann.ogasawara@canonical.com>

--- drivers/hid/hid-ntrig.c
+++ drivers/hid/hid-ntrig.c
@@ -825,7 +825,7 @@ static int ntrig_probe(struct hid_device *hdev, const struct hid_device_id *id)
 	struct input_dev *input;
 	struct hid_report *report;
 
-	if (id->driver_data)
+	if (id->driver_data & NTRIG_DUPLICATE_USAGES)
 		hdev->quirks |= HID_QUIRK_MULTI_INPUT;
 
 	nd = kmalloc(sizeof(struct ntrig_data), GFP_KERNEL);
