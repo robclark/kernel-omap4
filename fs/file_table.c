69ab2c1d5608bd264c90a2dfbded499c410d659d
Andy Whitcroft <apw@canonical.com>
2010-11-07 02:14:56 +0000

    UBUNTU: ubuntu: AUFS -- aufs2-standalone.patch aufs2.1-36-UNRELEASED-20101103
    Pull aufs up to the experimental aufs2.1-36-UNRELEASED-20101103
    from the URL below:
    
        http://livenet.selfip.com/ftp/debian/aufs2.1-36/
    
    Based on patches from J.R.Okajima and Jordi Pujol, see the mail thread
    below from the aufs-users list for further information:
    
        http://www.mail-archive.com/aufs-users@lists.sourceforge.net/msg02921.html
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- fs/file_table.c
+++ fs/file_table.c
@@ -393,6 +393,8 @@ void file_sb_list_del(struct file *file)
 	}
 }
 
+EXPORT_SYMBOL_GPL(file_sb_list_del);
+
 #ifdef CONFIG_SMP
 
 /*
--- security/commoncap.c
+++ security/commoncap.c
@@ -929,3 +929,4 @@ int cap_file_mmap(struct file *file, unsigned long reqprot,
 	}
 	return ret;
 }
+EXPORT_SYMBOL_GPL(cap_file_mmap);
