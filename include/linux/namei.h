c23555bab4e8b9a9787d34370a49bd2fb4968bfb
Andy Whitcroft <apw@canonical.com>
2010-11-07 02:11:12 +0000

    Revert "UBUNTU: ubuntu: AUFS -- aufs2 base patch for linux-2.6.34"
    This reverts commit 8e85bed37962e980e0a72cc3a8eb5fd9b16df630.
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- include/linux/namei.h
+++ include/linux/namei.h
@@ -79,9 +79,6 @@ extern int vfs_path_lookup(struct dentry *, struct vfsmount *,
 extern struct file *lookup_instantiate_filp(struct nameidata *nd, struct dentry *dentry,
 		int (*open)(struct inode *, struct file *));
 
-extern struct dentry *lookup_hash(struct nameidata *nd);
-extern int __lookup_one_len(const char *name, struct qstr *this,
-			    struct dentry *base, int len);
 extern struct dentry *lookup_one_len(const char *, struct dentry *, int);
 
 extern int follow_down_one(struct path *);
