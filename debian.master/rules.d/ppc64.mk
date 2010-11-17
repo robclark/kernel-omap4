dbb650ff022e4a0efc5c9bd17b4b3180650126bb
Andy Whitcroft <apw@canonical.com>
2010-11-17 09:52:33 +0000

    UBUNTU: ppc64 -- add basic architecture
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

---
+++ debian.master/rules.d/ppc64.mk
@@ -0,0 +1,19 @@
+build_arch	= powerpc
+header_arch	= $(build_arch)
+asm_link	= $(build_arch)
+defconfig	= ppc64_defconfig
+flavours	= powerpc64-smp
+build_image	= vmlinux
+kernel_file	= $(build_image)
+install_file	= $(build_image)
+
+loader		= yaboot
+
+custom_flavours	=
+
+no_dumpfile = true
+skipdbg		= true
+skipabi		= true
+skipmodule	= true
+
+family=ubuntu
