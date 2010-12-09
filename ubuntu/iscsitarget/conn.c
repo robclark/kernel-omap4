1138f8694059773658f2a8b796d5579b955861b6
Andy Whitcroft <apw@canonical.com>
2010-12-09 16:17:44 +0000

    UBUNTU: ubuntu: iscsitarget -- convert NIPQUAD to %pI4
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- ubuntu/iscsitarget/conn.c
+++ ubuntu/iscsitarget/conn.c
@@ -44,7 +44,7 @@ void conn_info_show(struct seq_file *seq, struct iscsi_session *session)
 		switch (sk->sk_family) {
 		case AF_INET:
 			snprintf(buf, sizeof(buf),
-				 "%u.%u.%u.%u", NIPQUAD(inet_sk(sk)->inet_daddr));
+				 "%pI4", &inet_sk(sk)->inet_daddr);
 			break;
 		case AF_INET6:
 			snprintf(buf, sizeof(buf), "[%pI6]",
