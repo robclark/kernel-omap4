8b83605986137673db1b301d28d027052d145211
Andy Whitcroft <apw@canonical.com>
2010-12-09 16:04:33 +0000

    UBUNTU: ubuntu: iscsitarget -- re-instate copy_io_context
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- ubuntu/iscsitarget/wthread.c
+++ ubuntu/iscsitarget/wthread.c
@@ -59,6 +59,19 @@ static int cmnd_execute(struct iscsi_cmnd *cmnd)
 	return target_type_array[type]->execute_cmnd(cmnd);
 }
 
+static void copy_io_context(struct io_context **pdst, struct io_context **psrc)
+{
+	struct io_context *src = *psrc;
+	struct io_context *dst = *pdst;
+
+	if (src) {
+		BUG_ON(atomic_long_read(&src->refcount) == 0);
+		atomic_long_inc(&src->refcount);
+		put_io_context(dst);
+		*pdst = src;
+	}
+}
+
 static int worker_thread(void *arg)
 {
 	struct worker_thread *wt = (struct worker_thread *) arg;
