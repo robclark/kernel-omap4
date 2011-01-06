4d0cd908cf562b39fb7f5b88712f31c78ce3e501
Archit Taneja <archit@ti.com>
2011-01-06 10:44:10 +0530

    OMAP2PLUS: DSS2: Make members of dss_clk_source generic
    The enum members of 'dss_clk_source' have clock source names specific to
    OMAP2/3. Change the names to more generic terms such that they now describe
    where the clocks come from and what they are used for.
    
    Also, change the enum member names to have "DSS_CLK_SRC" instead of "DSS_SRC"
    for more clarity.
    
    Signed-off-by: Archit Taneja <archit@ti.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/dss/dispc.c
+++ drivers/video/omap2/dss/dispc.c
@@ -2334,8 +2334,13 @@ unsigned long dispc_fclk_rate(void)
 {
 	unsigned long r = 0;
 
+<<<<<<< HEAD
 	if (dss_get_dispc_clk_source() == DSS_SRC_DSS1_ALWON_FCLK)
 		r = dss_clk_get_rate(DSS_CLK_FCK1);
+=======
+	if (dss_get_dispc_clk_source() == DSS_CLK_SRC_FCK)
+		r = dss_clk_get_rate(DSS_CLK_FCK);
+>>>>>>> OMAP2PLUS: DSS2: Make members of dss_clk_source generic
 	else
 #ifdef CONFIG_OMAP2_DSS_DSI
 		r = dsi_get_dsi1_pll_rate();
@@ -2385,7 +2390,7 @@ void dispc_dump_clocks(struct seq_file *s)
 	seq_printf(s, "- DISPC -\n");
 
 	seq_printf(s, "dispc fclk source = %s\n",
-			dss_get_dispc_clk_source() == DSS_SRC_DSS1_ALWON_FCLK ?
+			dss_get_dispc_clk_source() == DSS_CLK_SRC_FCK ?
 			"dss1_alwon_fclk" : "dsi1_pll_fclk");
 
 	seq_printf(s, "fck\t\t%-16lu\n", dispc_fclk_rate());
