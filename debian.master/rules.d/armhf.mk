d2c2e207e42f7ea861f6844fa0fe8330542c8031
Loïc Minier <loic.minier@linaro.org>
2011-03-16 01:30:17 +0100

    UBUNTU: Add initial armhf.mk
    Copy armel.mk for armhf, but disable all flavours for now.
    
    Signed-off-by: Loïc Minier <loic.minier@linaro.org>
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

---
+++ debian.master/rules.d/armhf.mk
@@ -0,0 +1,14 @@
+build_arch	= arm
+header_arch	= arm
+asm_link	= arm
+defconfig	= defconfig
+flavours	=
+build_image	= zImage
+kernel_file	= arch/$(build_arch)/boot/zImage
+install_file	= vmlinuz
+no_dumpfile = true
+
+loader		= grub
+
+skipabi		= true
+skipmodules	= true
