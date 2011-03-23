1321336b4e180675525d033547f3fdddc88dbcb9
Ricardo Neri <ricardo.neri@ti.com>
2011-03-23 13:36:43 -0600

    ASoC: OMAP: Add platform_device ID to SDP4430 machine driver
    Utilize a platform_device ID when creating the device. This is to
    allow to other SDP and Panda machine drivers to connect to soc-audio.
    
    New machine drivers must use different platform_device IDs if they
    need to coexist with existing ASoC machine drivers.
    
    Signed-off-by: Ricardo Neri <ricardo.neri@ti.com>

--- sound/soc/omap/sdp4430.c
+++ sound/soc/omap/sdp4430.c
@@ -39,6 +39,8 @@
 #include "omap-dmic.h"
 #include "../codecs/twl6040.h"
 
+#define SDP4430_SND_DEV_ID 0
+
 static int twl6040_power_mode;
 static int mcbsp_cfg;
 
@@ -897,7 +899,8 @@ static int __init sdp4430_soc_init(void)
 	}
 	printk(KERN_INFO "SDP4430 SoC init\n");
 
-	sdp4430_snd_device = platform_device_alloc("soc-audio", -1);
+	sdp4430_snd_device = platform_device_alloc("soc-audio",
+		SDP4430_SND_DEV_ID);
 	if (!sdp4430_snd_device) {
 		printk(KERN_ERR "Platform device allocation failed\n");
 		return -ENOMEM;
