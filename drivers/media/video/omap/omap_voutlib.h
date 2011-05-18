4abbccaf7e471de8319788919cb59277600e2bd3
Archit Taneja <archit@ti.com>
2011-05-18 14:41:03 +0530

    OMAP_VOUT: CLEANUP: Move some functions and macros from omap_vout
    Move some inline functions from omap_vout.c to omap_voutdef.h and independent
    functions like omap_vout_alloc_buffer/omap_vout_free_buffer to omap_voutlib.c.
    
    Signed-off-by: Archit Taneja <archit@ti.com>

--- drivers/media/video/omap/omap_voutlib.h
+++ drivers/media/video/omap/omap_voutlib.h
@@ -30,5 +30,7 @@ extern int omap_vout_new_window(struct v4l2_rect *crop,
 extern void omap_vout_new_format(struct v4l2_pix_format *pix,
 		struct v4l2_framebuffer *fbuf, struct v4l2_rect *crop,
 		struct v4l2_window *win);
+extern unsigned long omap_vout_alloc_buffer(u32 buf_size, u32 *phys_addr);
+extern void omap_vout_free_buffer(unsigned long virtaddr, u32 buf_size);
 #endif	/* #ifndef OMAP_VOUTLIB_H */
 
