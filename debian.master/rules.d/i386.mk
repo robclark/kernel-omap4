b2476061274539cecc19f72b044c8faae88bb2ac
Xavier Boudet <x-boudet@ti.com>
2012-02-20 13:21:03 +0100

    UBUNTU: Initial Debianization
    Signed-off-by: Xavier Boudet <x-boudet@ti.com>

---
+++ debian.master/rules.d/i386.mk
@@ -0,0 +1,10 @@
+build_arch	= i386
+header_arch	= x86_64
+asm_link	= x86
+defconfig	= defconfig
+flavours        = generic generic-pae virtual
+build_image	= bzImage
+kernel_file	= arch/$(build_arch)/boot/bzImage
+install_file	= vmlinuz
+loader		= grub
+no_dumpfile	= true
