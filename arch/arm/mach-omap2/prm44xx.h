5090a40bab11c91af6f129e3a9137d80862f3ff2
Rajendra Nayak <rnayak@ti.com>
2012-02-22 23:33:00 +0800

    RM: OMAP4 PM: Add IO Daisychain support
    patch adds IO Daisychain support for OMAP4 as per section 3.9.4 in OMAP4430
    Public TRM.
    
    Signed-off-by: Rajendra Nayak <rnayak@ti.com>
    Signed-off-by: Vishwanath BS <vishwanath.bs@ti.com>

--- arch/arm/mach-omap2/prm44xx.h
+++ arch/arm/mach-omap2/prm44xx.h
@@ -766,6 +766,7 @@ void omap4_prm_abb_clear_txdone(u8 abb_id);
 extern u32 omap4_prm_vcvp_read(u8 offset);
 extern void omap4_prm_vcvp_write(u32 val, u8 offset);
 extern u32 omap4_prm_vcvp_rmw(u32 mask, u32 bits, u8 offset);
+extern void omap4_trigger_wuclk_ctrl(void);
 
 /* PRM interrupt-related functions */
 extern void omap44xx_prm_read_pending_irqs(unsigned long *events);
