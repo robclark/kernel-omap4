81227a1a4a25ec41f8cb3fd4f3df8738e1ac5b03
Nishanth Menon <nm@ti.com>
2012-02-23 15:44:28 +0800

    OMAP3+: Smartreflex: Add a shutdown hook
     Since OMAP's VP and PRM modules do not get reset by warm reset,
     we should ensure that proper shutdown procedure is followed prior
     to allowing the kernel to reboot back up.
    
     Without this patch, Smartreflex module might be left active or
     system might be caught in an indeterminate sequence when reboot
     is triggered, leaving the next reboot behavior to be unpredictable.
    
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- arch/arm/mach-omap2/smartreflex.c
+++ arch/arm/mach-omap2/smartreflex.c
@@ -1403,7 +1403,7 @@ static void __devexit omap_sr_shutdown(struct platform_device *pdev)
 	}
 
 	if (sr_info->autocomp_active)
-		sr_stop_vddautocomp(sr_info);
+		sr_stop_vddautocomp(sr_info, true, 1);
 
 	return;
 }
