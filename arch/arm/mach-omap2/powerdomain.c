e333d2aa892f3a2feb4bc3dbe004b0fa8bdaa8dd
Axel Haslam <axelhaslam@ti.com>
2012-02-23 15:02:16 +0800

    OMAP4: PM: support ret_logic/mem_off_counters
    On OMAP4, there is no support to read previous logic state
    or previous memory state achieved when a power domain transitions
    to RET. Instead there are module level context registers.
    
    In order to support the powerdomain level logic/mem_off_counters
    on OMAP4, instead use the previous power state achieved (RET) and
    the *programmed* logic/mem RET state to derive if a powerdomain lost
    logic or did not.
    
    If the powerdomain is programmed to enter RET state and lose logic
    in RET state, knowing that the powerdomain entered RET is good enough
    to derive that the logic was lost as well, in such cases.
    
    Signed-off-by: Axel Haslam <axelhaslam@ti.com>
    [rnayak@ti.com: Updated changelog, updated patch to support mem counters]
    Signed-off-by: Rajendra Nayak <rnayak@ti.com>

--- arch/arm/mach-omap2/powerdomain.c
+++ arch/arm/mach-omap2/powerdomain.c
@@ -131,20 +131,28 @@ static int _pwrdm_register(struct powerdomain *pwrdm)
 static void _update_logic_membank_counters(struct powerdomain *pwrdm)
 {
 	int i;
-	u8 prev_logic_pwrst, prev_mem_pwrst;
+	int logic_pwrst, mem_pwrst;
+
+	logic_pwrst = pwrdm_read_prev_logic_pwrst(pwrdm);
+	if (logic_pwrst == -EINVAL)
+		logic_pwrst = pwrdm_read_logic_retst(pwrdm);
 
 	prev_logic_pwrst = pwrdm_read_prev_logic_pwrst(pwrdm);
-
+#if 0
 	/* Fake logic off counter */
 	if ((pwrdm->pwrsts_logic_ret == PWRSTS_OFF_RET) &&
 		(pwrdm_read_logic_retst(pwrdm) == PWRDM_POWER_OFF))
+#endif
+	if ((pwrdm->pwrsts_logic_ret == PWRSTS_OFF_RET) &&
+	    (logic_pwrst == PWRDM_POWER_OFF))
 		pwrdm->ret_logic_off_counter++;
 
 	for (i = 0; i < pwrdm->banks; i++) {
-		prev_mem_pwrst = pwrdm_read_prev_mem_pwrst(pwrdm, i);
-
+		mem_pwrst = pwrdm_read_prev_mem_pwrst(pwrdm, i);
+		if (mem_pwrst == -EINVAL)
+			mem_pwrst = pwrdm_read_mem_retst(pwrdm, i);
 		if ((pwrdm->pwrsts_mem_ret[i] == PWRSTS_OFF_RET) &&
-		    (prev_mem_pwrst == PWRDM_POWER_OFF))
+		    (mem_pwrst == PWRDM_POWER_OFF))
 			pwrdm->ret_mem_off_counter[i]++;
 	}
 }
