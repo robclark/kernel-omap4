548c2aea1844feea059633597c08cb046b73e243
Erik Rainey <erik.rainey@ti.com>
2012-04-20 13:02:55 -0500

    Updates to copyright and MODULE_DEVICE_TABLE. Also removed unneeded header

--- drivers/staging/omaprpc/omap_rpc.c
+++ drivers/staging/omaprpc/omap_rpc.c
@@ -1,19 +1,35 @@
 /*
- * OMAP Remote Procedure Call Driver
+ * OMAP Remote Procedure Call Driver.
  *
- * Copyright (C) 2011 Texas Instruments, Inc.
+ * Copyright(c) 2012 Texas Instruments. All rights reserved.
  *
- * Erik Rainey <erik.rainey@ti.com>
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
  *
- * This software is licensed under the terms of the GNU General Public
- * License version 2, as published by the Free Software Foundation, and
- * may be copied, distributed, and modified under those terms.
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in
+ *   the documentation and/or other materials provided with the
+ *   distribution.
+ * * Neither the name Texas Instruments nor the names of its
+ *   contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
 
 #include "omap_rpc_internal.h"
 
@@ -717,7 +733,8 @@ static ssize_t omaprpc_write(struct file *filp,
             goto failure;
         }
     }
-#if 0
+
+#if 0 // Very Verbose Debugging Code
     print_hex_dump(KERN_DEBUG, "OMAPRPC: TX: ", DUMP_PREFIX_NONE, 16, 1, kbuf, use, true);
 #endif
 
@@ -1058,7 +1075,7 @@ static struct rpmsg_device_id omaprpc_id_table[] = {
     { .name = "omaprpc" },
     { },
 };
-MODULE_DEVICE_TABLE(platform, omaprpc_id_table);
+MODULE_DEVICE_TABLE(rpmsg, omaprpc_id_table);
 
 static struct rpmsg_driver omaprpc_driver = {
     .drv.name   = KBUILD_MODNAME,
@@ -1139,6 +1156,6 @@ module_exit(omaprpc_fini);
 
 MODULE_AUTHOR("Erik Rainey <erik.rainey@ti.com>");
 MODULE_DESCRIPTION("OMAP Remote Procedure Call Driver");
-MODULE_ALIAS("platform:" MODULE_NAME);
+MODULE_ALIAS("rpmsg:omaprpc");
 MODULE_LICENSE("GPL v2");
 
