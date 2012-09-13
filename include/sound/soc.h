d1d524f5e078bbe5111176f6a0ebcc1e4486543e
Liam Girdwood <lrg@ti.com>
2012-09-13 12:37:11 +0100

    ASoC: core: Allow dynamic enum kcontrols.
    To be merged into firmware based kcontrol update.
    
    Signed-off-by: Liam Girdwood <lrg@ti.com>

--- include/sound/soc.h
+++ include/sound/soc.h
@@ -291,6 +291,10 @@
 #define SND_SOC_COMP_ORDER_LATE		1
 #define SND_SOC_COMP_ORDER_LAST		2
 
+#define snd_soc_get_enum_text(soc_enum, idx) \
+       (soc_enum->texts ? soc_enum->texts[idx] : soc_enum->dtexts[idx])
+
+
 /*
  * Bias levels
  *
@@ -1089,6 +1093,10 @@ struct soc_enum {
 	unsigned int mask;
 	const char * const *texts;
 	const unsigned int *values;
+	/* dynamic enum controls */
+	char **dtexts;
+	unsigned int *dvalues;
+
 	void *dapm;
 };
 
