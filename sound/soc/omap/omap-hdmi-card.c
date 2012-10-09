14da67d35093f4f8708e38df7b3f2cd23ab1b4c5
Ricardo Neri <ricardo.neri@ti.com>
2012-10-09 12:25:33 -0500

    ASoC: OMAP: HDMI: Update CPU DAI driver name
    Update driver name to use the device created by the OMAPDSS HDMI
    driver.
    
    Signed-off-by: Ricardo Neri <ricardo.neri@ti.com>

--- sound/soc/omap/omap-hdmi-card.c
+++ sound/soc/omap/omap-hdmi-card.c
@@ -37,7 +37,7 @@ struct hdmi_card_data {
 static struct snd_soc_dai_link omap_hdmi_dai = {
 	.name = "HDMI",
 	.stream_name = "HDMI",
-	.cpu_dai_name = "omap-hdmi-audio-dai",
+	.cpu_dai_name = "omap_hdmi_audio",
 	.platform_name = "omap-pcm-audio",
 	.codec_name = "hdmi-audio-codec",
 	.codec_dai_name = "omap-hdmi-hifi",
