98c9ccb6e727c4b545d189f24e14ffd033682f30
Rob Clark <rob@ti.com>
2012-10-10 11:37:51 -0500

    OMAPDSS: use mgr-id in output
    First part of seperating out omap_overlay/omap_overlay_manager into
    the apply layer.  The dispc and output parts really just care about
    mgr/ovl id, and not the entire omap_overlay/omap_overlay_manager
    state which belongs more to the apply layer.
    
    Signed-off-by: Rob Clark <rob@ti.com>

--- drivers/video/omap2/dss/dpi.c
+++ drivers/video/omap2/dss/dpi.c
@@ -129,7 +129,7 @@ static int dpi_set_dispc_clk(struct omap_dss_device *dssdev,
 static int dpi_set_mode(struct omap_dss_device *dssdev)
 {
 	struct omap_video_timings *t = &dpi.timings;
-	struct omap_overlay_manager *mgr = dssdev->output->manager;
+	enum omap_channel mgr = dssdev->output->manager_id;
 	int lck_div = 0, pck_div = 0;
 	unsigned long fck = 0;
 	unsigned long pck;
@@ -161,7 +161,7 @@ static int dpi_set_mode(struct omap_dss_device *dssdev)
 
 static void dpi_config_lcd_manager(struct omap_dss_device *dssdev)
 {
-	struct omap_overlay_manager *mgr = dssdev->output->manager;
+	enum omap_channel mgr = dssdev->output->manager_id;
 
 	dpi.mgr_config.io_pad_mode = DSS_IO_PAD_MODE_BYPASS;
 
@@ -188,7 +188,7 @@ int omapdss_dpi_display_enable(struct omap_dss_device *dssdev)
 		goto err_no_reg;
 	}
 
-	if (out == NULL || out->manager == NULL) {
+	if (out == NULL || out->manager_id == OMAP_DSS_CHANNEL_INVALID) {
 		DSSERR("failed to enable display: no output/manager\n");
 		r = -ENODEV;
 		goto err_no_out_mgr;
@@ -234,7 +234,7 @@ int omapdss_dpi_display_enable(struct omap_dss_device *dssdev)
 
 	mdelay(2);
 
-	r = dss_mgr_enable(out->manager);
+	r = dss_mgr_enable(out->manager_id);
 	if (r)
 		goto err_mgr_enable;
 
@@ -267,7 +267,7 @@ EXPORT_SYMBOL(omapdss_dpi_display_enable);
 
 void omapdss_dpi_display_disable(struct omap_dss_device *dssdev)
 {
-	struct omap_overlay_manager *mgr = dssdev->output->manager;
+	enum omap_channel mgr = dssdev->output->manager_id;
 
 	mutex_lock(&dpi.lock);
 
@@ -309,13 +309,13 @@ int dpi_check_timings(struct omap_dss_device *dssdev,
 			struct omap_video_timings *timings)
 {
 	int r;
-	struct omap_overlay_manager *mgr = dssdev->output->manager;
+	enum omap_channel mgr = dssdev->output->manager_id;
 	int lck_div, pck_div;
 	unsigned long fck;
 	unsigned long pck;
 	struct dispc_clock_info dispc_cinfo;
 
-	if (dss_mgr_check_timings(mgr, timings))
+	if (!dispc_mgr_timings_ok(mgr, timings))
 		return -EINVAL;
 
 	if (timings->pixel_clock == 0)
