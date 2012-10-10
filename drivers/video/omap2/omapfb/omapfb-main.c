229b369c25bbd864ee1d5256193b836f431653e4
Tomi Valkeinen <tomi.valkeinen@ti.com>
2012-10-10 10:26:45 +0300

    OMAPDSS: use omapdss_compat_init() in other drivers
    omapdss_compat_init() and omapdss_compat_uninit() is called internally
    by omapdss. This patch moves the calls to omapfb, omap_vout and omapdrm
    drivers. omapdrm driver can later remove the call after non-compat
    support has been implemented in omapdrm.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/omap2/omapfb/omapfb-main.c
+++ drivers/video/omap2/omapfb/omapfb-main.c
@@ -2438,6 +2438,9 @@ static int __init omapfb_probe(struct platform_device *pdev)
 				"ignoring the module parameter vrfb=y\n");
 	}
 
+	r = omapdss_compat_init();
+	if (r)
+		goto err0;
 
 	mutex_init(&fbdev->mtx);
 
@@ -2561,6 +2564,7 @@ static int __init omapfb_probe(struct platform_device *pdev)
 
 cleanup:
 	omapfb_free_resources(fbdev);
+	omapdss_compat_uninit();
 err0:
 	dev_err(&pdev->dev, "failed to setup omapfb\n");
 	return r;
@@ -2575,6 +2579,8 @@ static int __exit omapfb_remove(struct platform_device *pdev)
 	omapfb_remove_sysfs(fbdev);
 
 	omapfb_free_resources(fbdev);
+
+	omapdss_compat_uninit();
 
 	return 0;
 }
