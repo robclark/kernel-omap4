d4da6290736342a321f54932fc1c5bce051f3fc4
Pantelis Antoniou <panto@antoniou-consulting.com>
2012-10-13 16:34:44 +0300

    omap: Export omap_hwmod_lookup/omap_device_build/omap_device_build_ss
    These functions can be used just fine by modules, there's no need not
    to have them exported.

--- arch/arm/plat-omap/omap_device.c
+++ arch/arm/plat-omap/omap_device.c
@@ -663,7 +663,7 @@ void omap_device_delete(struct omap_device *od)
  * information.  Returns ERR_PTR(-EINVAL) if @oh is NULL; otherwise,
  * passes along the return value of omap_device_build_ss().
  */
-struct platform_device __init *omap_device_build(const char *pdev_name, int pdev_id,
+struct platform_device *omap_device_build(const char *pdev_name, int pdev_id,
 				      struct omap_hwmod *oh, void *pdata,
 				      int pdata_len,
 				      struct omap_device_pm_latency *pm_lats,
@@ -678,6 +678,7 @@ struct platform_device __init *omap_device_build(const char *pdev_name, int pdev
 				    pdata_len, pm_lats, pm_lats_cnt,
 				    is_early_device);
 }
+EXPORT_SYMBOL(omap_device_build);
 
 /**
  * omap_device_build_ss - build and register an omap_device with multiple hwmods
@@ -696,7 +697,7 @@ struct platform_device __init *omap_device_build(const char *pdev_name, int pdev
  * platform_device record.  Returns an ERR_PTR() on error, or passes
  * along the return value of omap_device_register().
  */
-struct platform_device __init *omap_device_build_ss(const char *pdev_name, int pdev_id,
+struct platform_device *omap_device_build_ss(const char *pdev_name, int pdev_id,
 					 struct omap_hwmod **ohs, int oh_cnt,
 					 void *pdata, int pdata_len,
 					 struct omap_device_pm_latency *pm_lats,
@@ -751,6 +752,7 @@ odbs_exit:
 
 	return ERR_PTR(ret);
 }
+EXPORT_SYMBOL(omap_device_build_ss);
 
 /**
  * omap_early_device_register - register an omap_device as an early platform
